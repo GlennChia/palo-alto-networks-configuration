f38bdf4938e1696316f5e8f3e999b802011d3a91
GlennChia <glennchia7@gmail.com>
2020-08-07 13:41:13 +0800

    Added allowed IPs for management and deny IPs

--- utility.sh
+++ utility.sh
@@ -19,7 +19,11 @@ read_var() {
 phashReplace=$(read_var phashReplace)
 publicKeyReplace=$(read_var publicKeyReplace)
 sysLogIp=$(read_var sysLogIp)
+allowManagementIp=$(read_var allowManagementIp)
+denyManagementIp=$(read_var denyManagementIp)
 
 sed -i "s/phashReplace/${phashReplace}/" palo_alto_config
 sed -i "s/publicKeyReplace/${publicKeyReplace}/" palo_alto_config
 sed -i "s/sysLogIp/${sysLogIp}/" palo_alto_config
+sed -i "s+allowManagementIp+${allowManagementIp}+" palo_alto_config
+sed -i "s/denyManagementIp/${denyManagementIp}/" palo_alto_config
